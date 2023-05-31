0266ad68c6a341049b61a95bfb1224183261c2d0
rittik sheikh <rittik11500@gamil.com>
2023-05-31 20:20:00 +0600

    daisyui color customization done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,32 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#FF912C",
+
+          "secondary": "#FF3811",
+
+          "accent": "#29B170",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F77272",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
