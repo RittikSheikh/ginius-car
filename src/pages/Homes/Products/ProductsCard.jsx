50bdf49697d77456ea5f226d54e0775193da3ce5
rittik sheikh <rittik11500@gamil.com>
2023-06-02 08:07:18 +0600

    products cards align items center done

--- src/pages/Homes/Products/ProductsCard.jsx
+++ src/pages/Homes/Products/ProductsCard.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { AiOutlineArrowRight } from 'react-icons/ai';
+import { AiFillStar } from 'react-icons/ai';
 const ProductsCard = ({data}) => {
     const {img, price,description, title, _id} = data;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl border-2 border-[#E8E8E8] ">
-            <div className='p-6'><img className='w-full md:h-[210px] rounded-lg' src={img} alt="" /></div>
-            <div className="card-body">
-                <h2 className="card-title font-bold text-2xl">{title}</h2>
-                <div className='flex justify-between'>
-                <p className='text-secondary font-semibold text-xl'>Price: ${price}</p>
-                <AiOutlineArrowRight className='text-2xl text-secondary'></AiOutlineArrowRight>
+            <div className='bg-[#F3F3F3] m-5 rounded-lg'><img className='w-full p-2 md:h-[210px]' src={img} alt="" /></div>
+            <div className="card-body items-center mb-5">
+                <div className='flex text-primary'>
+                <AiFillStar></AiFillStar>
+                <AiFillStar></AiFillStar>
+                <AiFillStar></AiFillStar>
+                <AiFillStar></AiFillStar>
+                <AiFillStar></AiFillStar>
                 </div>
+                <h2 className="card-title font-bold text-2xl">{title}</h2>
+                <p className='text-secondary font-semibold text-xl'>Price: ${price}</p>
             </div>
         </div>
     );
